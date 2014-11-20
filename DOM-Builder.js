c2d5ee97d95571659978b7b7301dd0ef5819938d
Daniel Hug <danielthehug@gmail.com>
2014-11-20 11:15:35 -0800

    Add AMD exporting

--- DOM-Builder.js
+++ DOM-Builder.js
@@ -1,48 +1,60 @@
 // http://jsbin.com/DOMBuilder/2/edit
-var DOM = {
-	buildNode: function buildNode(nodeData) {
-		// create text node
-		if (typeof nodeData === 'string')
-			return document.createTextNode(nodeData);
-
-		// nodeData is already a DOM node
-		if (nodeData.appendChild) return nodeData;
-
-		// create element
-		var el = document.createElement(nodeData.el || 'div');
-
-		for (var attr in nodeData) {
-			if (['el', 'kid', 'kids'].indexOf(attr) === -1) {
-				// set JS properties
-				if (attr[0] === '_') el[attr.slice(1)] = nodeData[attr];
-
-				// add event listeners
-				else if (attr.slice(0,3) === 'on_') {
-					var eventName = attr.slice(3);
-					var handlers = nodeData[attr];
-					for (var i = 0; i < handlers.length; i++) el.addEventListener(eventName, handlers[i]);
-				}
-
-				// add html attributes
-				else el.setAttribute(attr, nodeData[attr]);
-			}
-		}
-
-		// add child nodes
-		if (nodeData.kid) el.appendChild(buildNode(nodeData.kid));
-		else if (nodeData.kids) el.appendChild($.DOM.buildDocFrag(nodeData.kids));
-
-		return el;
-	},
-
-	buildDocFrag: function buildDocFrag(arr) {
-		var docFrag = document.createDocumentFragment();
-
-		// build each node and stick in docFrag
-		arr.forEach(function appendEach(nodeData) {
-			docFrag.appendChild($.DOM.buildNode(nodeData));
-		});
-
-		return docFrag;
+(function (root, factory) {
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(factory);
+	} else {
+		// Browser globals
+		root.DOM = factory();
 	}
-};
+})(this, function() {
+	'use strict';
+	return {
+		buildNode: function buildNode(nodeData) {
+			// create text node
+			if (typeof nodeData === 'string')
+				return document.createTextNode(nodeData);
+
+			// nodeData is already a DOM node
+			if (nodeData.appendChild) return nodeData;
+
+			// create element
+			var el = document.createElement(nodeData.el || 'div');
+
+			for (var attr in nodeData) {
+				if (['el', 'kid', 'kids'].indexOf(attr) === -1) {
+					// set JS properties
+					if (attr[0] === '_') el[attr.slice(1)] = nodeData[attr];
+
+					// add event listeners
+					else if (attr.slice(0,3) === 'on_') {
+						var eventName = attr.slice(3);
+						var handlers = nodeData[attr];
+						for (var i = 0; i < handlers.length; i++) el.addEventListener(eventName, handlers[i]);
+					}
+
+					// add html attributes
+					else el.setAttribute(attr, nodeData[attr]);
+				}
+			}
+
+			// add child nodes
+			if (nodeData.kid) el.appendChild(buildNode(nodeData.kid));
+			else if (nodeData.kids) el.appendChild($.DOM.buildDocFrag(nodeData.kids));
+
+			return el;
+		},
+
+		buildDocFrag: function buildDocFrag(arr) {
+			var docFrag = document.createDocumentFragment();
+
+			// build each node and stick in docFrag
+			arr.forEach(function appendEach(nodeData) {
+				docFrag.appendChild($.DOM.buildNode(nodeData));
+			});
+
+			return docFrag;
+		}
+	};
+});
