6dd31a76e2e4bdc38180c5d541cb5f2e6794a376
Daniel Hug <daniel@hugwebdesign.com>
2015-10-28 17:24:46 -0700

    merge buildNode and buildDocFrag into dom()

--- DOM-Builder.js
+++ DOM-Builder.js
@@ -2,65 +2,62 @@
 (function (root, factory) {
 	'use strict';
 	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
+		// anonymous AMD module
 		define(factory);
 	} else {
 		// Browser globals
-		root.DOM = factory();
+		root.dom = factory();
 	}
 })(this, function() {
 	'use strict';
-	var DOM = {
-		buildNode: function buildNode(nodeData) {
-			// create text node
-			if (typeof nodeData === 'string')
-				return document.createTextNode(nodeData);
+	return function dom(nodeData) {
+		// string -> text node
+		if (typeof nodeData === 'string')
+			return document.createTextNode(nodeData);
 
-			// nodeData is already a DOM node
-			if (nodeData.appendChild) return nodeData;
+		// DOM node -> DOM node
+		if (nodeData instanceof Node) return nodeData;
 
-			// create element
-			var el = document.createElement(nodeData.el || 'div');
-
-			for (var attr in nodeData) {
-				if (['el', 'kid', 'kids'].indexOf(attr) === -1) {
-					// set JS properties
-					if (attr[0] === '_') el[attr.slice(1)] = nodeData[attr];
-
-					// add event listeners
-					else if (attr.slice(0, 3) === 'on_') {
-						var eventName = attr.slice(3);
-						var handlers = nodeData[attr];
-
-						// accept a function:
-						if (typeof handlers === 'function') handlers = [handlers];
-
-						// or an array of functions:
-						for (var i = 0; i < handlers.length; i++) el.addEventListener(eventName, handlers[i], false);
-					}
-
-					// add html attributes
-					else el.setAttribute(attr, nodeData[attr]);
-				}
-			}
-
-			// add child nodes
-			if (nodeData.kid) el.appendChild(buildNode(nodeData.kid));
-			else if (nodeData.kids) el.appendChild(DOM.buildDocFrag(nodeData.kids));
-
-			return el;
-		},
-
-		buildDocFrag: function buildDocFrag(arr) {
-			var docFrag = document.createDocumentFragment();
-
+		// array -> document fragment
+		if (Array.isArray(nodeData)) {
 			// build each node and stick in docFrag
-			arr.forEach(function appendEach(nodeData) {
-				docFrag.appendChild(DOM.buildNode(nodeData));
+			var docFrag = document.createDocumentFragment();
+			nodeData.forEach(function(nodeData) {
+				docFrag.appendChild(dom(nodeData));
 			});
 
 			return docFrag;
 		}
+
+		// object -> element
+		var el = document.createElement(nodeData.el || 'div');
+
+		for (var attr in nodeData) {
+			if (['el', 'kid', 'kids'].indexOf(attr) === -1) {
+				// set JS properties
+				if (attr[0] === '_') el[attr.slice(1)] = nodeData[attr];
+
+				// add event listeners
+				else if (attr.slice(0, 3) === 'on_') {
+					var eventName = attr.slice(3);
+					var handlers = nodeData[attr];
+
+					// accept a function
+					if (typeof handlers === 'function') handlers = [handlers];
+
+					// or an array of functions
+					for (var i = 0; i < handlers.length; i++) el.addEventListener(eventName, handlers[i], false);
+				}
+
+				// add html attributes
+				else el.setAttribute(attr, nodeData[attr]);
+			}
+		}
+
+		// add child nodes
+		if (nodeData.kid) el.appendChild(dom(nodeData.kid));
+		else if (nodeData.kids) el.appendChild(dom(nodeData.kids));
+
+		return el;
 	};
-	return DOM;
 });
