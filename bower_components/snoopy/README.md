cc62d6e0d3713470e297d10e7e0f06526d2cd1e7
Daniel Hug <daniel@hugwebdesign.com>
2016-01-23 09:35:56 -0800

    update snoopy

--- bower_components/snoopy/README.md
+++ bower_components/snoopy/README.md
@@ -1,7 +1,7 @@
 # snoopy
 Observable objects in JS
 
-## usage
+## example
 
 ```js
 // setup observable data
@@ -17,6 +17,59 @@ counter.snoop('even', function(even) {
 	console.log(this.counter + ' is ' + (even ? 'even' : 'odd') + '.');
 });
 ```
+
+## constructor and method usage
+
+### `Snoopy()`
+
+Snoopy is a constructor but the `new` keyword is optional. Call it passing an optional object with some initial properties and values:
+
+```js
+var counter = new Snoopy({count: 0});
+```
+
+### `Snoopy.prototype.set(property, value)`
+
+```js
+var person = Snoopy();
+person.set('name', 'Hank');
+```
+
+### `Snoopy.prototype.get(property)`
+
+There are two ways to get the value of a property on a Snoopy instance:
+
+1. `person.get('name');`
+2. `person.name`
+
+### `Snoopy.prototype.snoop(property, mapFn)`
+
+**arguments**  
+
+Here are the possible ways to call `.snoop()`:
+
+```js
+person.snoop('name')(function snooper(name) {
+	console.log('name is now ' + name);
+})
+```
+
+```js
+person.snoop('name', function snooper(name) {
+	console.log('name is now ' + name);
+})
+```
+
+```js
+person.snoop('firstName lastName', function map(firstName, lastName) {
+	return firstName + ' ' + lastName;
+})(function snooper(name) {
+	console.log('name is now ' + name);
+})
+```
+
+**return value**  
+When `.snoop()` is called, it returns a "snoopable" function. The snoopable function accepts a callback, calls it right away passing the values of the properties being snooped, and calls it again whenever one of the values change.
 
 ## use with [DOM Builder](https://github.com/Daniel-Hug/DOM-Builder)
 
--- bower_components/snoopy/snoopy.js
+++ bower_components/snoopy/snoopy.js
@@ -1,7 +1,7 @@
 var Snoopy = (function() {
 	function Snoopy(obj) {
 		if (!(this instanceof Snoopy)) return new Snoopy(obj);
-		// don't overwrite this!:
+		// don't overwrite this.snoopers!
 		this.snoopers = {};
 		Object.keys(obj).forEach(function(key) {
 			this[key] = obj[key];
@@ -12,10 +12,10 @@ var Snoopy = (function() {
 		var newVal = this[prop];
 		(this.snoopers[prop] || []).forEach(function(snooper) {
 			snooper.call(this, newVal);
-		});
+		}, this);
 	};
 
-	var get = Snoopy.prototype.get = function(prop) {
+	Snoopy.prototype.get = function(prop, val) {
 		return this[prop];
 	};
 
@@ -24,14 +24,19 @@ var Snoopy = (function() {
 		changed.call(this, prop);
 	};
 
-	Snoopy.prototype.snoop = function(props, snooper) {
-		if (snooper) {
+	Snoopy.prototype.snoop = function(props, map) {
+		if (map) {
+			var snooper;
+			var mapOutput;
+			var propToVal = this.get.bind(this);
 			var propsArr = props.split(' ');
 
-			// caller calls snooper with the values of the passed props
-			var propToVal = get.bind(this);
+			// caller calls map with the values of the passed props
+			// then, if there is a snooper, calls that passing the value returned from the map 
 			var caller = function caller() {
-				snooper.apply(null, propsArr.map(propToVal));
+				var propVals = propsArr.map(propToVal);
+				mapOutput = map.apply(null, propVals);
+				if (snooper) snooper(mapOutput);
 			};
 
 			caller();
@@ -42,6 +47,11 @@ var Snoopy = (function() {
 				if (!t.snoopers[prop]) t.snoopers[prop] = [];
 				t.snoopers[prop].push(caller);
 			});
+
+			return function mappedSnoop(newSnooper) {
+				snooper = newSnooper;
+				snooper(mapOutput);
+			};
 		} else {
 			// snoopable
 			return this.snoop.bind(this, props);
